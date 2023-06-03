37983d237a00204c6dcc5153cfb3f39943d6c7c3
Kazao TM <m.jumari@gmail.com>
2023-06-04 01:56:28 +0700

    - change config

--- expressjs/js/app.js
+++ expressjs/js/app.js
@@ -1,8 +1,8 @@
 $(async () => {
 
     const SIJKT_URL = "https://sijkt.inovesia.co.id/sso";
-    const SIJKT_NUMBER = "1685791343049";
-    const SIJKT_KEY = "MTY4NTc5MTM1NjMyNDI5ODUyMS1RazE0M2dRRg==";
+    const SIJKT_NUMBER = "APPLICATION_NUMBER";
+    const SIJKT_KEY = "APPLICAION_KEY";
 
     console.log('Login');
     $('#connect').click((e) => {
